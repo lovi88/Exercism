4b6b74ca3ec038c783b771f52b2cece266b2c70e
lovi88 <l.pista@hotmail.com>
2017-10-15 10:31:40 +0200

    Sum of multiplies first iteration.

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        ISet<int> results = new HashSet<int>();
+
+        foreach (var multiple in multiples)
+        {
+            for (int i = 1; ; i++)
+            {
+                var multiplied = i * multiple;
+
+                if (multiplied >= max)
+                {
+                    break;
+                }
+
+                results.Add(multiplied);
+            }
+        }
+
+        return results.Sum();
+    }
+}
