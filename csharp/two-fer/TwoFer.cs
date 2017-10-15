54494121557713d8e616b0062a387061b61d7c58
lovi88 <l.pista@hotmail.com>
2017-10-15 11:13:02 +0200

    Two-fer first iteration.

---
+++ csharp/two-fer/TwoFer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class TwoFer
+{
+    public static string Name(string input = null)
+    {
+        var name = string.IsNullOrEmpty(input)
+            ? "you"
+            : input;
+
+        return $"One for {name}, one for me.";
+    }
+}
