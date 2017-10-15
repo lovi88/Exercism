fc8b12c029b623ea73f61915ca93495fca8c25f5
lovi88 <l.pista@hotmail.com>
2017-10-15 11:15:53 +0200

    Two-fer second iteration.

--- csharp/two-fer/TwoFer.cs
+++ csharp/two-fer/TwoFer.cs
@@ -1,13 +1,5 @@
-﻿using System;
-
-public static class TwoFer
+﻿public static class TwoFer
 {
     public static string Name(string input = null)
-    {
-        var name = string.IsNullOrEmpty(input)
-            ? "you"
-            : input;
-
-        return $"One for {name}, one for me.";
-    }
+        => $"One for {input ?? "you"}, one for me.";
 }
