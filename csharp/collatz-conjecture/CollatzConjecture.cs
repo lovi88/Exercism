9dd4add9cfc7cbbb70048165d33a925d2e7f02f2
lovi88 <l.pista@hotmail.com>
2017-10-21 13:52:08 +0200

    Collatz Conjecture first iteration.

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CollatzConjecture
+{
+    public static class CollatzConjecture
+    {
+        public static int Steps(int number)
+        {
+            ValidateInputs();
+
+            return MakeSteps();
+
+            int MakeSteps()
+            {
+                var times = 0;
+
+                while (number != 1)
+                {
+                    times++;
+
+                    number = number % 2 == 0
+                        ? number / 2
+                        : number * 3 + 1;
+                }
+
+                return times;
+            }
+
+            void ValidateInputs()
+            {
+                if (number < 1)
+                {
+                    throw new ArgumentException($"{nameof(number)} must be more or equal to one.");
+                }
+            }
+        }
+
+    }
+}
