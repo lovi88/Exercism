755ce12a9dcea6358d6be8200ca25f0ab40a6aaa
lovi88 <l.pista@hotmail.com>
2017-10-21 15:14:59 +0200

    KindergartenGarden kata added.

---
+++ csharp/kindergarten-garden/KindergartenGarden.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KindergartenGarden
+{
+    public enum Plant
+    {
+        Violets,
+        Radishes,
+        Clover,
+        Grass
+    }
+
+    public class KindergartenGarden
+    {
+        private readonly List<string> _students;
+        private readonly List<List<Plant>> _rows;
+
+        public KindergartenGarden(string diagram)
+            : this(diagram, new[]{ "Alice", "Bob", "Charlie", "David", "Eve", "Fred",
+                "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry" })
+        { }
+
+        public KindergartenGarden(string diagram, IEnumerable<string> students)
+        {
+            _students = students.OrderBy(s => s).ToList();
+            _rows = diagram.Split("\n").Select(ToPlants).ToList();
+        }
+
+        public IEnumerable<Plant> Plants(string student)
+        {
+            var rowIdx = _students.IndexOf(student) * 2;
+            return _rows.Select(row => new[] { row[rowIdx], row[rowIdx + 1] }).SelectMany(plants => plants);
+        }
+
+        private static List<Plant> ToPlants(string str) => str.Select(ToPlant).ToList();
+
+        private static Plant ToPlant(char str)
+        {
+            switch (str)
+            {
+                case 'V':
+                    return Plant.Violets;
+                case 'R':
+                    return Plant.Radishes;
+                case 'C':
+                    return Plant.Clover;
+                case 'G':
+                    return Plant.Grass;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
+}
