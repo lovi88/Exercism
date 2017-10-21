9d84f9633de1e3545b867250bb78105ad61f7e64
lovi88 <l.pista@hotmail.com>
2017-10-21 10:41:31 +0200

    Accumulate second iteration

--- csharp/accumulate/Accumulate.cs
+++ csharp/accumulate/Accumulate.cs
@@ -5,7 +5,5 @@ using System.Linq;
 public static class AccumulateExtensions
 {
     public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
-    {
-        return collection.Select(func);
-    }
+        => collection.Select(func);
 }
